d3742580530cc4c68ee9176ed316767cddb0ffea
Divyanshu Dwivedi <divyanshudwivedi2018gmail.com@Divyanshus-MacBook-Air.local>
2024-05-18 17:59:12 +0530

    cow

---
+++ kalloc.c
@@ -0,0 +1,255 @@
+// Physical memory allocator, intended to allocate
+// memory for user processes, kernel stacks, page table pages,
+// and pipe buffers. Allocates 4096-byte pages.
+
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "spinlock.h"
+
+
+struct{
+  int use_lock;
+  struct spinlock lock;
+  int ref_count[PHYSTOP/PGSIZE];
+  long long process_idxs[PHYSTOP/PGSIZE];
+}rmap;
+
+void initrmap(){
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  for(int ind = 0; ind< (PHYSTOP/PGSIZE);++ind){
+    rmap.ref_count[ind] = 0;
+    rmap.process_idxs[ind] = 0;
+  }
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+
+int 
+get_rmap(uint pa)
+{ 
+  int rmap_index;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  int value=rmap.ref_count[rmap_index];
+  if(rmap.use_lock)
+    release(&rmap.lock);
+  return value;
+}
+void 
+set_rmap(uint pa,int value)
+{
+  int rmap_index;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  rmap.ref_count[rmap_index]=value;
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+void 
+inc_rmap(uint pa)
+{
+  int rmap_index;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  rmap.ref_count[rmap_index]++;
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+void 
+dec_rmap(uint pa)
+{
+  int rmap_index;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  if(rmap.ref_count[rmap_index] > 0)
+    rmap.ref_count[rmap_index]--;
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+
+int get_pindex_status(uint pa, uint p_index)
+{
+  int rmap_index, pindex_status=0;
+  long long val=1;
+  if(p_index>=64 || p_index<0)
+    panic("wrong pindex in get_pindex_status");
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  if((rmap.process_idxs[rmap_index])&(val<<p_index))
+    pindex_status=1;
+  if(rmap.use_lock)
+    release(&rmap.lock);
+  return pindex_status;
+}
+
+long long get_pindex_value(uint pa){
+  uint rmap_index;
+  long long process_idxs;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  process_idxs=rmap.process_idxs[rmap_index];
+  if(rmap.use_lock)
+    release(&rmap.lock);
+  return process_idxs;
+}
+
+void set_pindex_value(uint pa, long long process_idxs){
+  uint rmap_index;
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  rmap.process_idxs[rmap_index] = process_idxs;
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+
+void set_pindex_status(uint pa, uint p_index,uint pindex_status)
+{
+  int rmap_index;
+  long long  val=1;
+  if(p_index>=64 || p_index<0)
+    panic("wrong pindex in set_pindex_status");
+
+  if(rmap.use_lock)
+    acquire(&rmap.lock);
+  rmap_index = ((pa>>PTXSHIFT)&(0xFFFFF));
+  if(pindex_status)
+  {
+    rmap.process_idxs[rmap_index] |=(val<<p_index);
+  }
+  else{
+    rmap.process_idxs[rmap_index] &=(~(val<<p_index));
+  }
+  if(rmap.use_lock)
+    release(&rmap.lock);
+}
+
+void freerange(void *vstart, void *vend);
+extern char end[]; // first address after kernel loaded from ELF file
+                   // defined by the kernel linker script in kernel.ld
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  int use_lock;
+  uint num_free_pages;  //store number of free pages
+  struct run *freelist;
+} kmem;
+
+// Initialization happens in two phases.
+// 1. main() calls kinit1() while still using entrypgdir to place just
+// the pages mapped by entrypgdir on free list.
+// 2. main() calls kinit2() with the rest of the physical pages
+// after installing a full page table that maps them on all cores.
+void
+kinit1(void *vstart, void *vend)
+{
+  initlock(&kmem.lock, "kmem");
+  kmem.use_lock = 0;
+  rmap.use_lock=0;
+  freerange(vstart, vend);
+}
+
+void
+kinit2(void *vstart, void *vend)
+{
+  freerange(vstart, vend);
+  kmem.use_lock = 1;
+  rmap.use_lock=1;
+}
+
+void
+freerange(void *vstart, void *vend)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint)vstart);
+  for(; p + PGSIZE <= (char*)vend; p += PGSIZE)
+  {
+    kfree(p);
+    // kmem.num_free_pages+=1;
+  }
+    
+}
+//PAGEBREAK: 21
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(char *v)
+{
+  struct run *r;
+  // cprintf(" page address %p \n",v);
+  if((uint)v % PGSIZE || v < end || V2P(v) >= PHYSTOP)
+    panic("kfree");
+
+  dec_rmap(V2P(v));
+  if(get_rmap(V2P(v))>0)
+    return;
+
+  // Fill with junk to catch dangling refs.
+  memset(v, 1, PGSIZE);
+
+  if(kmem.use_lock)
+    acquire(&kmem.lock);
+  r = (struct run*)v;
+  r->next = kmem.freelist;
+  kmem.num_free_pages+=1;
+  kmem.freelist = r;
+  if(kmem.use_lock)
+    release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+char*
+kalloc(void)
+{
+  struct run *r;
+
+  if(kmem.use_lock)
+    acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+  {
+    kmem.freelist = r->next;
+    kmem.num_free_pages-=1;
+    set_rmap(V2P(r),1); 
+  }
+  if(kmem.use_lock)
+    release(&kmem.lock);
+  
+  if(!r)
+  {
+    swap_out();
+    // cprintf("page allocated when r==0\n");
+    return kalloc();
+  }
+  // cprintf("page address that allocated:%p\n",(char*)r);
+  return (char*)r;
+}
+uint 
+num_of_FreePages(void)
+{
+  acquire(&kmem.lock);
+
+  uint num_free_pages = kmem.num_free_pages;
+  
+  release(&kmem.lock);
+  
+  return num_free_pages;
+}
---
+++ paging.c
@@ -0,0 +1,218 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "fs.h"
+#include "buf.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "memlayout.h"
+
+struct slotinfo slot_array[SSIZE];
+uint swap_start;
+
+void initpageswap(uint sbstart){
+    swap_start=sbstart;
+    for(uint i=0;i<SSIZE;i++)
+    {
+        slot_array[i].is_free=1;
+    }
+}
+
+void pagingintr(){
+    // cprintf("Debug: Pageing Intrupt Handler \n");
+    struct proc *curproc;
+    uint pfa, pa, flags;
+    pte_t *pte;
+    char *mem;
+    // Accesing the process that caused pagefault
+    curproc=myproc();
+    // Accessing the page addr that caused the pagefault 
+    pfa=rcr2();
+    // Accessing the page table entry of the fault page.
+    pte=walkpgdir(curproc->pgdir, (void *)pfa,0);
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+
+    // Checking is pagefault due to COW
+    if((flags & PTE_P))
+    {
+      // cprintf("Debug: inside present side\n");
+      if((mem = kalloc()) == 0)
+        panic("memory is not available for page copy");
+      memmove(mem, (char*)P2V(pa), PGSIZE);
+      flags|=PTE_W;
+      *pte= PTE_ADDR((uint)V2P(mem)) | flags;
+      kfree(P2V(pa));
+    }
+    else{
+      cprintf("Debug: Inside swap in side\n");
+      swap_in();
+    }
+    return;
+}
+
+void free_swap(struct proc* p)
+{
+//   cprintf("Inside free swap %d:\n",p->pid);
+  uint pindx = find_proc_index(p);
+  long long mask = 1<<pindx;
+//   cprintf("freed pid %d:\n",p->pid);
+  for(int i=0;i<SSIZE;i++)
+  {
+    if(((slot_array[i].pindex & mask) == mask) && (slot_array[i].is_free==0))
+    {
+      slot_array[i].pindex = (slot_array[i].pindex & (~mask));
+      slot_array[i].ref_count--;
+      if(slot_array[i].ref_count <= 0){
+        slot_array[i].ref_count = 0;
+        slot_array[i].is_free = 1;
+      }
+    }
+  }
+}
+uint write_to_swap(char* vpa)
+{
+   uint slot_ind=0;
+   for(slot_ind=0;slot_ind<SSIZE;slot_ind++)
+   {
+      if(slot_array[slot_ind].is_free)
+      {
+        slot_array[slot_ind].is_free=0;
+        break;
+      }
+   }
+   if(slot_ind==SSIZE) slot_ind--;
+   uint block_num=swap_start+slot_ind*8;
+   for(int i=0;i<8;i++)
+   {
+        struct buf* b=bread(ROOTDEV,block_num+i);
+        memmove(b->data,vpa,512);
+        b->blockno=block_num+i;
+        b->flags|=B_DIRTY;
+        b->dev=ROOTDEV;
+        bwrite(b);
+        brelse(b);
+        vpa=vpa+512;
+   } 
+   return swap_start+slot_ind*8;
+}
+
+void read_from_swap(uint sbn, char* pa)
+{
+    for(int i=0;i<8;i++)
+    {
+        struct buf* b=bread(ROOTDEV,sbn+i);
+        memmove(pa,b->data,512);
+        brelse(b);
+        pa+=512;
+    }
+    slot_array[sbn/8].is_free=1;
+}
+
+void
+swap_in(){
+    long long pindxs;
+    uint pfa, flags, ref_cnt, sbn, pgaddr;
+    char* pa;
+    pte_t* pte;
+    struct proc* curproc;
+
+    curproc=myproc();
+    
+    pfa=rcr2();
+    pte=walkpgdir(curproc->pgdir, (void *)pfa,0);
+    // cprintf("Debug: page table entry %p\n",*pte);
+    sbn=(*pte)>>PTXSHIFT;
+    pa = kalloc();
+    pgaddr = (V2P(pa)&(~0xFFF));
+    
+    flags = slot_array[sbn/8].page_perm;
+    ref_cnt = slot_array[sbn/8].ref_count;
+    pindxs = slot_array[sbn/8].pindex;
+    read_from_swap(sbn, pa);
+    // cprintf("Debug: page table entry %p\n",*pte);
+    swap_in_update_pte_for_pindex(pindxs, pfa, pgaddr, flags);
+    (*pte)= pgaddr | flags| PTE_P;
+    // cprintf("Debug: page table entry %p\n",*pte);
+    set_pindex_value(pgaddr, pindxs);
+    set_rmap(pgaddr,ref_cnt);
+
+    // Updating 
+    // cprintf("process id thats page swapped in: %d and  page table entry %p \n",curproc->pid,(*pte));
+}
+
+void swap_in_update_pte_for_pindex(long long pindx, uint vaddr, uint pg, uint flags){
+  long long mask=1;
+  struct proc* curProc;
+  pte_t* curPte;
+  for(int indx = 0; indx < 64; ++indx){
+    if((pindx & (mask<<indx))){
+      curProc = find_proc_from_index(indx);
+      curPte = walkpgdir(curProc->pgdir, (void *)vaddr,0);
+      (*curPte) = pg | flags | PTE_P;
+      curProc->rss+=4096;
+    }
+  }
+}
+void
+swap_out(){
+    struct proc* p;
+    struct pageinfo pg;
+    pte_t* pte;
+    uint vaddr;
+    uint pa, flags;
+    char* vpa; 
+    long long pindxs;
+    
+    p = find_victim_process();
+    pg = find_victim_page(p);
+
+    // cprintf("\nDebug Victim IPD [%d], Page [%p]\n", p->pid, *pg);
+    // cprintf("\n  Debug Victim IPD [%d], Page [%p]\n", p->pid, pg);
+    while(pg.pte == 0)
+    {
+        // cprintf("Hello i am going to unaccessed some pages.\n");
+        make_unaccessed_page(p);
+        pg=find_victim_page(p);
+    }
+
+    pte = pg.pte;
+    vaddr = pg.vaddr;
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    vpa=(char*)(P2V(pa));    
+    
+    // cprintf("   victim page %p:\n",pa);
+    uint sbn = write_to_swap(vpa);
+    pindxs = get_pindex_value(*pte);
+    slot_array[sbn/8].page_perm = flags;
+    slot_array[sbn/8].pindex = pindxs;
+    slot_array[sbn/8].ref_count = get_rmap(pa);
+
+    swap_out_update_pte_for_pindex(pindxs, vaddr, sbn);
+    (*pte)=((sbn<<PTXSHIFT)&(~0xfff));
+    set_pindex_value(pa, 0);
+    set_rmap(pa,1);
+    kfree(vpa);
+    // cprintf("\n  Debug Victim IPD [%d], Page [%p]\n", p->pid, *pg);
+    //p->rss-=4096;
+    // cprintf("  process id thats page swapped out: %d \n",p->pid);
+} 
+
+
+void swap_out_update_pte_for_pindex(long long pindx, uint vaddr, uint sbn){
+  long long mask=1;
+  struct proc* curProc;
+  pte_t* curPte;
+  for(int indx = 0; indx < 64; ++indx){
+    if((pindx & (mask<<indx))){
+      curProc = find_proc_from_index(indx);
+      curPte = walkpgdir(curProc->pgdir, (void *)vaddr,0);
+      (*curPte) = ((sbn<<PTXSHIFT)&(~0xFFF));
+      curProc->rss-=4096;
+    }
+  } 
+}
